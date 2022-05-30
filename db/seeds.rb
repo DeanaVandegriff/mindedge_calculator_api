8437272aa0bf099bd80b27f845c0ddc24b90c942
deana <dvandegriff1@gmail.com>
2022-05-30 13:19:33 -0500

    adds additional seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Calculation.create(value1: 2, operator: "+", value2: 2, equals: "=", result: 4)
+
+# Calculation.create(value1: 10, operator: "-", value2: 2, equals: "=", result: 8)
+
+# Calculation.create(value1: 5, operator: "*", value2: 2, equals: "=", result: 10)
+
+# Calculation.create(value1: 10, operator: "/", value2: 5, equals: "=", result: 2)
