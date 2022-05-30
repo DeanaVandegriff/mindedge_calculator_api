a29b03959a70521b87dec27d00b75fe0f4ad9f19
deana <dvandegriff1@gmail.com>
2022-05-30 11:23:43 -0500

    adds routes for calculation controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/calculations" => "calculations#index"
+  # get "/calculations/:id" => "calculations#show"
+  post "/calculations" => "calculations#create"
+  # patch "/calculations/:id" => "calculations#update"
+  # delete "/calculations/:id" => "calculations#destroy"
 end
