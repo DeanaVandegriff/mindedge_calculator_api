bd5b34deedf4f7ed9e51ba96d453303e47cf59ec
deana <dvandegriff1@gmail.com>
2022-05-30 11:33:19 -0500

    adds index and create action

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,2 +1,18 @@
 class CalculationsController < ApplicationController
+  def index
+    calculations = Calculation.all
+    render json: calculations.as_json
+  end
+
+  def create
+    calculation = Calculation.new(
+      value1: params[:value1],
+      operator: params[:operator],
+      value2: params[:value2],
+      equals: params[:equals],
+      result: params[:result],
+    )
+    calculation.save
+    render json: calculation.as_json
+  end
 end
